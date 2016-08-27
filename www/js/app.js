7315cb7e401f3645d6799c3c006bec6ccf1b9b1f
Rishabh Bhatia <rishabh.bhatia08@gmail.com>
2016-08-28 00:14:58 +0530

    Tabs top fixes, Dummy home, messages & profile tabs

--- www/js/app.js
+++ www/js/app.js
@@ -22,3 +22,44 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+
+    // $ionicConfigProvider.tabs.position('bottom');
+
+    $stateProvider
+    .state('tabs', {
+        url: '/tab',
+        abstract: true,
+        templateUrl: 'templates/tabs.html'
+    })
+
+     .state('tabs.home', {
+      url: '/home',
+      views: {
+        'home-tab': {
+          templateUrl: 'templates/home.html'
+        }
+      }
+    })
+
+     .state('tabs.messages', {
+      url: '/messages',
+      views: {
+        'messages-tab': {
+          templateUrl: 'templates/messages.html'
+        }
+      }
+    })
+
+     .state('tabs.profile', {
+      url: '/profile',
+      views: {
+        'profile-tab': {
+          templateUrl: 'templates/profile.html'
+        }
+      }
+    })
+
+    $urlRouterProvider.otherwise('/tab/home');
+})
