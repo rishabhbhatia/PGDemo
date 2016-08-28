5f4232b88f542aebda78b1cd65a43ff25ac772e9
Rishabh Bhatia <rishabh.bhatia08@gmail.com>
2016-08-28 22:28:23 +0530

    Login Page, Login Service & Login empty input validator

--- www/js/app.js
+++ www/js/app.js
@@ -1,20 +1,9 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
       cordova.plugins.Keyboard.disableScroll(true);
     }
     if(window.StatusBar) {
@@ -26,6 +15,13 @@ angular.module('starter', ['ionic'])
 .config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
 
     // $ionicConfigProvider.tabs.position('bottom');
+
+    $stateProvider
+    .state('login', {
+        url: '/login',
+        templateUrl: 'templates/login.html',
+        controller: 'LoginController'
+    })
 
     $stateProvider
     .state('tabs', {
@@ -64,8 +60,40 @@ angular.module('starter', ['ionic'])
       }
     })
 
-    $urlRouterProvider.otherwise('/tab/home');
+    // $urlRouterProvider.otherwise('/tab/home');
+    $urlRouterProvider.otherwise('/login');
 })
+
+.controller("LoginController", function($scope, $http, $state, $ionicPopup) {
+  $scope.data = {};
+
+  $scope.login = function() {
+    if($scope.data.username === undefined ||
+     $scope.data.password === undefined) 
+    {
+      var alertPopup = $ionicPopup.alert({
+                title: 'Invalid input',
+                template: 'Please enter your credentials!'
+            });
+      return;
+    }
+  console.log("Please log me in. I'm "+$scope.data.username+
+    " & my password is: "+$scope.data.password);
+
+    /*  LoginService.loginUser($scope.data.username, $scope.data.password).
+      success(function(data) {
+            $state.go('tab.dash');
+        }).error(function(data) {
+            var alertPopup = $ionicPopup.alert({
+                title: 'Login failed!',
+                template: 'Please check your credentials!'
+            });
+        });*/
+
+      }
+
+})
+
 
 .controller("PartyController", [ "$scope" , "$http", "$state" ,
   function($scope, $http, $state) {
